98ce0d3da05b3bb9cfdd0341c4346a7f92307fcc
Arvindraj <arvindultimate7352@gmail.com>
2018-09-29 21:15:48 +0530

    Change Main Card content; Let's be social!

---
+++ static/js/main.js
@@ -0,0 +1,15 @@
+let githubIcon = document.getElementById("github-id");
+let linkedinIcon = document.getElementById("linkedin-id");
+let telegramIcon = document.getElementById("telegram-id");
+
+githubIcon.onclick = function() {
+    window.open("https://github.com/a7r3");
+};
+
+linkedinIcon.onclick = function() {
+    window.open("https://www.linkedin.com/in/arvindraj-thangaraj-434656164/");
+};
+
+telegramIcon.onclick = function() {
+    window.open("https://t.me/I_Iz_N00b");
+}
