83d72e8cbd85ef1e053af37cfca37e030b7e05a7
Arvindraj <arvindultimate7352@gmail.com>
2018-10-01 01:06:45 +0530

    Add Tab Navigation
    Signed-off-by: Arvindraj <arvindultimate7352@gmail.com>

--- static/js/main.js
+++ static/js/main.js
@@ -13,3 +13,25 @@ linkedinIcon.onclick = function() {
 telegramIcon.onclick = function() {
     window.open("https://t.me/I_Iz_N00b");
 }
+
+let navigateItem = function(event, tabItem) {
+    let i = 0;
+    let tabContent = document.getElementsByClassName("tab-container");
+
+    for(i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+
+    document.getElementById(tabItem).style.display = "block";
+
+    let tabItems = document.getElementsByClassName("tab-item");
+
+    for(i = 0; i < tabItems.length; i++) {
+        tabItems[i].className = tabItems[i].className.replace(" active", "");
+    }
+
+    event.currentTarget.className += " active";
+};
+
+// Select 'About' as the default-selected tab
+document.getElementById("defaultOpen").click();
