8c6c2dee7c4d828e571396bd1f8ac5f45d64f385
Arvindraj <arvindultimate7352@gmail.com>
2020-05-16 21:30:13 +0530

    Dedicated assets folder pls
    Signed-off-by: Arvindraj <arvindultimate7352@gmail.com>

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,7 @@
 function loadJSON(callback) {   
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open('GET', '../../whats-up.json', true);
+    xobj.open('GET', '../../assets/whats-up.json', true);
     xobj.onreadystatechange = function () {
           if (xobj.readyState == 4 && xobj.status == "200") {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
