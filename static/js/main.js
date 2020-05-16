020bdbb52ab02f3e428706c03b013976ec1c73bd
Arvindraj <arvindultimate7352@gmail.com>
2020-05-16 15:26:09 +0530

    Add Photos | Change Design, go for all-vertical layout | Scrap Tabs & Go for a blog-like section
    All-Vertical Layout: so that it can be mobile-friendly as well
    
    Signed-off-by: Arvindraj <arvindultimate7352@gmail.com>

--- static/js/main.js
+++ static/js/main.js
@@ -2,6 +2,42 @@ let githubIcon = document.getElementById("github-id");
 let linkedinIcon = document.getElementById("linkedin-id");
 let telegramIcon = document.getElementById("telegram-id");
 
+function loadJSON(callback) {   
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', '../../whats-up.json', true);
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+}
+
+let whatsupContent;
+
+var listItemString = $('#whatsupItem').html();
+var index = 0;
+
+function loadWhatsupContent(whatsup) {
+    var listItem = $('<li id=\'' + index + '\' class=\'whatsup-list-template\'>' + listItemString + '</li>');
+    $('.whatsup-title', listItem).html(whatsup.title);
+    $('.whatsup-description', listItem).html(whatsup.description);
+    $('#whatsupImage', listItem).attr('src', whatsup.image);
+    console.log(listItem.html)
+    $('#whatsupList').append(listItem);
+    index++;
+}
+
+loadJSON(function(text) {
+    whatsupContent = JSON.parse(text)
+    whatsupContent.forEach(loadWhatsupContent);
+    $('li').click(function() {
+        window.open(whatsupContent[this.id].link, '_blank');
+    });
+});
+
 githubIcon.onclick = function() {
     window.open("https://github.com/a7r3");
 };
@@ -13,25 +49,3 @@ linkedinIcon.onclick = function() {
 telegramIcon.onclick = function() {
     window.open("https://t.me/I_Iz_N00b");
 }
-
-let navigateItem = function(event, tabItem) {
-    let i = 0;
-    let tabContent = document.getElementsByClassName("tab-container");
-
-    for(i = 0; i < tabContent.length; i++) {
-        tabContent[i].style.display = "none";
-    }
-
-    document.getElementById(tabItem).style.display = "block";
-
-    let tabItems = document.getElementsByClassName("tab-item");
-
-    for(i = 0; i < tabItems.length; i++) {
-        tabItems[i].className = tabItems[i].className.replace(" active", "");
-    }
-
-    event.currentTarget.className += " active";
-};
-
-// Select 'About' as the default-selected tab
-document.getElementById("defaultOpen").click();
