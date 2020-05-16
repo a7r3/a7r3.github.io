ad6a718654c4de7109b286bc12158b8a2474e358
Arvindraj <arvindultimate7352@gmail.com>
2020-05-16 21:16:35 +0530

    social-container: Use anchor tag instead of div
    Signed-off-by: Arvindraj <arvindultimate7352@gmail.com>

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,3 @@
-let githubIcon = document.getElementById("github-id");
-let linkedinIcon = document.getElementById("linkedin-id");
-let telegramIcon = document.getElementById("telegram-id");
-
 function loadJSON(callback) {   
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
@@ -37,15 +33,3 @@ loadJSON(function(text) {
         window.open(whatsupContent[this.id].link, '_blank');
     });
 });
-
-githubIcon.onclick = function() {
-    window.open("https://github.com/a7r3");
-};
-
-linkedinIcon.onclick = function() {
-    window.open("https://www.linkedin.com/in/arvindraj-thangaraj-434656164/");
-};
-
-telegramIcon.onclick = function() {
-    window.open("https://t.me/I_Iz_N00b");
-}
